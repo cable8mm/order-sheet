f40d61cedc5dc58887398a5350d151d62fc4e1d3
Sam Lee <cable8mm@gmail.com>
2025-02-07 15:42:24 +0900

    Set up phpunit test framework

---
+++ tests/PhpUnitTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Cable8mm\OrderSheetInvoiceGenerator\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+final class PhpUnitTest extends TestCase
+{
+    public function test_phpunit_working(): void
+    {
+        $this->assertTrue(true);
+    }
+}
